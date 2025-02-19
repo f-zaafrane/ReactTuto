75ff67959c1d33de7205fab72add2f9bd5761b35
FaderFtw <fady.zaafrane@gmail.com>
2025-02-19 13:17:15 +0100

    Add initial project structure with API, frontend, and assets

---
+++ src/Pizza.jsx
@@ -0,0 +1,21 @@
+// This is Pure JS
+// const Pizza = (props) => {
+//   return React.createElement("div", {}, [
+//     React.createElement("h1", {}, props.name),
+//     React.createElement("p", {}, props.description),
+//   ]);
+// };
+
+
+// This is JSX
+const Pizza = (props) => {
+  return (
+    <div className="pizza">
+      <h1>{props.name}</h1>
+      <p>{props.description}</p>
+      <img src={props.image} alt={props.name} />
+    </div>
+  );
+};
+
+export default Pizza;
